38b71186d41c16a2fe6262493311688978b2101f
loster719 <jan-niklaswortmann@gmx.net>
2015-04-01 19:45:38 +0200

    json library and first implementations for login request but not ready:D

--- src/Server.java
+++ src/Server.java
@@ -8,15 +8,23 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.lang.reflect.Method;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @ServerEndpoint("/websocket")
 public class Server {
 
     @OnMessage
-    public void onMessage(String message, Session session) throws IOException, InterruptedException {
+    public void onMessage(String message, Session session) throws IOException, InterruptedException, JSONException {
         System.out.println("User input: " + message + "; Session: " + session);
         session.getBasicRemote().sendText("Hello world Mr. " + message);
+        JSONObject json_payload = new JSONObject(message);
 
+        //Method method = getClass().getDeclaredMethod(functionName);
+        //method.invoke(this);
         // Sending message to client each 1 second
         for (int i = 0; i <= 25; i++) {
             session.getBasicRemote().sendText(i + " Message from server");
@@ -34,5 +42,9 @@ public class Server {
     @OnClose
     public void onClose() {
         System.out.println("Connection closed");
+    }
+
+    private void login_user(){
+
     }
 }
