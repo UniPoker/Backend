435ae6eae8a5ec8c0cb57a1f1b23ff66ef28b332
loster719 <jan-niklaswortmann@gmx.net>
2015-07-23 15:33:18 +0200

    test class for interface

---
+++ src/main/java/server/PokerInterfaceTest.java
@@ -0,0 +1,22 @@
+package server;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by loster on 23.07.2015.
+ */
+public class PokerInterfaceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testReceive() throws Exception {
+
+    }
+}
